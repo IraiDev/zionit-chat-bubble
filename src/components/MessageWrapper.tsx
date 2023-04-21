702da85ff86c44b9478e8ef9d82d0c537d48af15
Ignacio arriagada <iarriagada@zionit.cl>
2023-04-21 18:20:26 -0400

    version 0.4.6 -fix alto de messageWrapper

--- src/components/MessageWrapper.tsx
+++ src/components/MessageWrapper.tsx
@@ -1,35 +1,55 @@
-import React, { useLayoutEffect, useRef } from "react"
-import { Message } from "./Message"
-import { useChatContext } from "../store/ChatStore"
-import { IMessage } from "../models/chat.model"
-import { DisconnectedWrapper } from "./DisconnectedWrapper"
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { Message } from './Message';
+import { useChatContext } from '../store/ChatStore';
+import { IMessage } from '../models/chat.model';
+import { DisconnectedWrapper } from './DisconnectedWrapper';
 
 interface Props {
-  messages: IMessage[]
+  messages: IMessage[];
 }
 
 export const MessageWrapper = ({ messages = [] }: Props) => {
-  const isFirstLoad = useRef(0)
-  const containerRef = useRef<HTMLDivElement>(null)
-  const { loggedUser, isConnected } = useChatContext()
+  const isFirstLoad = useRef(0);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const { loggedUser, isConnected } = useChatContext();
+  const [isKeyboardVisible, setIsKeyboardVisible] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const windowHeight = window.visualViewport
+        ? window.visualViewport.height
+        : window.innerHeight;
+      const documentHeight = document.documentElement.clientHeight;
+
+      setIsKeyboardVisible(windowHeight < documentHeight);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   useLayoutEffect(() => {
-    const container = containerRef.current
+    const container = containerRef.current;
 
-    if (container === null && messages.length === 0) return
+    if (container === null && messages.length === 0) return;
 
     container?.scrollTo({
       top: container.scrollHeight,
-      behavior: isFirstLoad.current < 2 ? "auto" : "smooth",
-    })
+      behavior: isFirstLoad.current < 2 ? 'auto' : 'smooth',
+    });
 
-    isFirstLoad.current = isFirstLoad.current + 1
-  }, [messages])
+    isFirstLoad.current = isFirstLoad.current + 1;
+  }, [messages]);
   return (
     <section
       ref={containerRef}
-      className="
-      w-80 sm:w-96 p-2 px-4 h-[400px] sm:h-[500px] scroll-app bg-cover bg-[url('https://assets.zproduccion.cl/chat/bg-wsp-light.jpg')] dark:bg-[url('https://assets.zproduccion.cl/chat/bg-wsp-dark.jpg')]"
+      className={`
+      w-80 sm:w-96 p-2 px-4 scroll-app bg-cover bg-[url('https://assets.zproduccion.cl/chat/bg-wsp-light.jpg')] dark:bg-[url('https://assets.zproduccion.cl/chat/bg-wsp-dark.jpg')] transition-all duration-200
+      ${isKeyboardVisible ? 'h-[30vh]' : 'h-[400px] sm:h-[470px]'}
+      `}
     >
       <DisconnectedWrapper hidden={isConnected} />
       <ul className="flex flex-col gap-2.5 w-full">
@@ -44,5 +64,5 @@ export const MessageWrapper = ({ messages = [] }: Props) => {
         ))}
       </ul>
     </section>
-  )
-}
+  );
+};
---
+++ src/typings.d.ts
@@ -0,0 +1,3 @@
+interface Window {
+  visualViewport: any;
+}
